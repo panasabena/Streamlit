3753d246e2a9aa93f1e5ca4316bded65fd92105c
Alfredo Sabena <alfredo.sabena@mi.unc.edu.ar>
2022-12-12 00:45:03 -0300

    Data Summary

--- proyecto2/pages/01_👨‍💻_Data Summary.py
+++ proyecto2/pages/01_👨‍💻_Data Summary.py
@@ -1,2 +1,13 @@
 import streamlit as st
-st.write('Hello')
+import pandas as pd
+st.title('Data Summary')
+st.text('This part contains the statistical info of our data')
+#st.markdown('### This is a **markdown**')
+## File opener: Muestra un drag and drop de archivos
+df = pd.read_csv('./Datasets/Recorrido_de_bicicletas_2015-2019.csv')
+
+## Funcion creada para cuando se seleccione la opcion de "Data Statistics"
+## Muestra df.describe ---> Las descripciones estadísticas básicas de un dataset
+def stats(df):
+    st.header('Data Statistics')
+    st.write(df.describe())
